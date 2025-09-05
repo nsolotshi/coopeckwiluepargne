10fd5e26f6a6d9b7167c2b6f1b9565452a3a0291
nsolotshi <nsolotshi3@gmail.com>
2025-09-05 16:28:42 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'coopec-kwilu-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://i.postimg.cc/RNH2v9sC/logo-coopec-Kwilu.jpg'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Retourne la réponse en cache ou fetch la requête
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Nettoyage des anciens caches
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
